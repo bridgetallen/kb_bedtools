596dd18574888519ec98e00100eab6ac1c63948d
Bridget Allen <ac.ballen@dev1.chicago.kbase.us>
2025-07-29 17:29:18 +0000

    wip 005

--- test/kb_bedtools_server_test.py
+++ test/kb_bedtools_server_test.py
@@ -84,8 +84,8 @@ class kb_bedtoolsTest(unittest.TestCase):
 
     def test_intersect(self):
         # in the test, use print() to put things in stdout
-        first_file = 'first_file'
-        second_file = 'second_file'
+        first_file = 'GSE240325_apo_rbfox_insitu_clustered.sorted.filtered_lite.gff'
+        second_file = 'GSE203496_xmoo1_line_pooled_assembly.gff'
         self.serviceImpl.run_kb_bedtools_intersect(
             self.ctx,
             {
